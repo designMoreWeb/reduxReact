3f943b1c8c3dbd091b02a73f63296aaabcb5fd60
designMoreWeb <divystolia@gmail.com>
2017-08-21 19:37:53 -0400

    Settings

--- redux-course/src/index.js
+++ redux-course/src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const state = {
+  todos: []
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
