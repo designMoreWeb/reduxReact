5c500180cd923047cab3a8b8536f4dc54a814030
designMoreWeb <divystolia@gmail.com>
2017-08-21 22:08:49 -0400

    Added rendering based on global object state

--- redux-course/src/index.js
+++ redux-course/src/index.js
@@ -5,8 +5,12 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 const state = {
-  todos: []
+  todos: [
+    { id: 1, name: "Render Static UI", isComplete: true },
+    { id: 2, name: "Create Intial State", isComplete: true },
+    { id: 3, name: "Use State to Render UI", isComplete: true }
+  ]
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App todos={state.todos} />, document.getElementById("root"));
 registerServiceWorker();
